1f155ac2bad8c32e88d56238f24f5e11275e58d3
Yadira Zeballos <yadira@tecsup.edu.pe>
2025-11-27 22:49:51 -0500

    comit2

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,16 +1,40 @@
-const BASE_URL = "https://mi-sitio.com";
+const BASE_URL = "https://mi-proyecto-seo-wk3k.onrender.com";
+
+async function obtenerRutasDinamicas() {
+  const blogs = [
+    { slug: "optimizacion-seo" },
+    { slug: "mejorar-rendimiento-nextjs" },
+    { slug: "lazy-loading-componentes" }
+  ];
+
+  return blogs.map((item) => `/blog/${item.slug}`);
+}
 
 export default async function handler(req, res) {
-  const urls = ["/", "/blog", "/contacto"]; // Agrega más rutas dinámicamente
+  const rutasEstaticas = [
+    "/",
+    "/contacto",
+    "/blog",
+  ];
+
+  const rutasDinamicas = await obtenerRutasDinamicas();
+
+  const todasLasRutas = [...rutasEstaticas, ...rutasDinamicas];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
-      .join("")}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${todasLasRutas
+  .map((url) => {
+    return `<url>
+      <loc>${BASE_URL}${url}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>`;
+  })
+  .join("")}
+</urlset>
+  `;
 
   res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+  res.status(200).send(sitemap);
 }
